09be02de1f541da4f86cbd62c4259040bdbc3e88
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2021-11-11 22:11:17 +0100

    APIs

---
+++ c5.py
@@ -0,0 +1,18 @@
+import requests
+from requests import Response
+
+# response = requests.get("https://semosedu.com.mk")
+#
+# print(response.status_code)
+# print(response.headers)
+#
+# print(response.content)
+#
+
+# response = requests.get('https://api.ipify.org?format=json')
+# response = response.json()
+#
+# print(type(response))
+# print(response.get("ip"))
+# # print(response.status_code)
+# # print(response.headers)
